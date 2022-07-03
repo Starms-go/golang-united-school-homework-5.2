22a5aa2597e8630b9d78ca9104e6963560260cde
Starms-go <evra3487@gmainl.com>
2022-07-04 02:11:05 +0300

    first attempt

--- cache.go
+++ cache.go
@@ -3,21 +3,57 @@ package cache
 import "time"
 
 type Cache struct {
+	m map[string]Val
+}
+
+type Val struct {
+	v   string
+	exp time.Time
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{m: map[string]Val{}}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (receiver Cache) Get(key string) (string, bool) {
+	for k, v := range receiver.m {
+		if !(v.exp.IsZero()) && v.exp.Before(time.Now()) {
+			delete(receiver.m, k)
+		}
+	}
+	if v, ok := receiver.m[key]; ok {
+		return v.v, ok
+	}
+	return "", false
 }
 
-func (receiver) Put(key, value string) {
+func (receiver Cache) Put(key, value string) {
+	for k, v := range receiver.m {
+		if !(v.exp.IsZero()) && v.exp.Before(time.Now()) {
+			delete(receiver.m, k)
+		}
+	}
+	receiver.m[key] = Val{v: value, exp: time.Time{}}
 }
 
-func (receiver) Keys() []string {
+func (receiver Cache) Keys() []string {
+	for k, v := range receiver.m {
+		if !(v.exp.IsZero()) && v.exp.Before(time.Now()) {
+			delete(receiver.m, k)
+		}
+	}
+	var keys []string
+	for k := range receiver.m {
+		keys = append(keys, k)
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (receiver Cache) PutTill(key, value string, deadline time.Time) {
+	for k, v := range receiver.m {
+		if !(v.exp.IsZero()) && v.exp.Before(time.Now()) {
+			delete(receiver.m, k)
+		}
+	}
+	receiver.m[key] = Val{v: value, exp: deadline}
 }
